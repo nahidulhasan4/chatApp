fc8565491ffb252d3c9a35db97ecc7fb34bff18c
nahidulhasan4 <nahidhasan4836@gmail.com>
2024-11-29 21:05:52 +0600

    here Block list

--- src/component/BlockList.jsx
+++ src/component/BlockList.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import Signinimg from "../assets/signin.jpg";
+import Signinimg from "../assets/signin.jpg"; 
 
 const BlockList = () => {
   return (
     <div className=" w-[427px]  shadow-xl rounded-[20px] px-5 mt-[43px] ">
       <div className="flex  justify-between items-center">
         <h2 className=" text-[20px] font-semibold text-black ">Friend List</h2>
-        <BsThreeDotsVertical />
+       
       </div>
 
       <div className=" w-full  h-[347px] overflow-y-scroll rounded-[20px] ">
@@ -29,7 +28,7 @@ const BlockList = () => {
             </div>
           </div>
           <button className=" bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg ">
-          add Friend
+          block
           </button>
         </div>
         <div className=" flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
@@ -50,7 +49,29 @@ const BlockList = () => {
             </div>
           </div>
           <button className=" bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg ">
-          add Friend
+          Block
+          </button>
+        </div>
+        
+        <div className=" flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
+          <div className=" flex items-center gap-[14px] ">
+            <img
+              className=" w-[70px] h-[70px] rounded-full"
+              src={Signinimg}
+              alt=""
+            />
+            <div>
+              <h3 className=" text-[18px] font-semibold text-black ">
+                Friends Reunion
+              </h3>
+              <p className=" text-[14px] font-semibold text-gray-500 ">
+                {" "}
+                Hi Guys, Wassup!
+              </p>
+            </div>
+          </div>
+          <button className=" bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg ">
+          Block
           </button>
         </div>
         <div className=" flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
@@ -71,7 +92,7 @@ const BlockList = () => {
             </div>
           </div>
           <button className=" bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg ">
-            add Friend
+          Block
           </button>
         </div>
         <div className=" flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
@@ -92,49 +113,7 @@ const BlockList = () => {
             </div>
           </div>
           <button className=" bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg ">
-          add Friend
-          </button>
-        </div>
-        <div className=" flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
-          <div className=" flex items-center gap-[14px] ">
-            <img
-              className=" w-[70px] h-[70px] rounded-full"
-              src={Signinimg}
-              alt=""
-            />
-            <div>
-              <h3 className=" text-[18px] font-semibold text-black ">
-                Friends Reunion
-              </h3>
-              <p className=" text-[14px] font-semibold text-gray-500 ">
-                {" "}
-                Hi Guys, Wassup!
-              </p>
-            </div>
-          </div>
-          <button className=" bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg ">
-          add Friend
-          </button>
-        </div>
-        <div className=" flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
-          <div className=" flex items-center gap-[14px] ">
-            <img
-              className=" w-[70px] h-[70px] rounded-full"
-              src={Signinimg}
-              alt=""
-            />
-            <div>
-              <h3 className=" text-[18px] font-semibold text-black ">
-                Friends Reunion
-              </h3>
-              <p className=" text-[14px] font-semibold text-gray-500 ">
-                {" "}
-                Hi Guys, Wassup!
-              </p>
-            </div>
-          </div>
-          <button className=" bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg ">
-          add Friend
+          Block
           </button>
         </div>
       </div>
